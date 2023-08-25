46cd57b467efb0b8621a8fb5a500d18faf918486
yaroslawv-nexus <124203359+yaroslawv-nexus@users.noreply.github.com>
2023-08-25 20:31:44 +0300

    startwork

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactsLIst/ContactList';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addContact = contact => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, { id: nanoid(), ...contact }],
+    }));
+  };
+
+  filterChange = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <ContactForm addContact={this.addContact} />
+        <ContactList
+          contacts={this.state.contacts}
+          filterText={this.state.filter}
+          filterChange={this.filterChange}
+        />
+      </div>
+    );
+  }
+}
